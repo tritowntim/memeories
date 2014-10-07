259a3bbe3b920da2d4b9c18bbc3ebbecceb0a29c
Tim Dussinger <tritowntim@gmail.com>
2014-10-06 21:47:05 -0400

    ember-rails

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+gem 'ember-rails'
+
 group :development, :test do
   gem 'rspec-rails', '~> 2.99.0'
 end
--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module Memeory
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # ember-rails
+    config.ember.app_name = 'App'
   end
 end
